e2b13a374bf063b1437d0cfe6930f435be949e14
susmoose <susannak@rams.colostate.edu>
2019-03-02 18:33:41 -0700

    base classes add

---
+++ workspaceDS/P2/src/main/java/cs455/scaling/client/Client.java
@@ -0,0 +1,10 @@
+package cs455.scaling.client;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("Client says hey");
+
+	}
+
+}
---
+++ workspaceDS/P2/src/main/java/cs455/scaling/server/Server.java
@@ -0,0 +1,9 @@
+package cs455.scaling.server;
+
+public class Server {
+
+	public static void main(String[] args) {
+		System.out.println("Server says hey");
+	}
+
+}
